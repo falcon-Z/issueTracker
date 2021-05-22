25f0b79ff3fa24201c13209a8f90fcedc446d5d1
falcon-Z <saimadhan@outlook.in>
2021-05-22 09:57:22 +0530

    first comit

---
+++ server/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+
+app.use(express.static('public'));
+
+app.listen(
+    3000, () => {
+        console.log('Listening on http://localhost:3000');
+    }
+);
