3c15c33e9166c6b1f61fb596ca6aec8417d16472
falcon-Z <saimadhan@outlook.in>
2021-05-24 10:22:08 +0530

    added graphql

--- server/app.js
+++ server/app.js
@@ -1,9 +1,31 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const fs = require('fs');
 
 const app = express();
 
+let aboutMessage = "Issue Tracker API v1.0";
+
+const resolvers = {
+    Query: {
+        about: () => aboutMessage,
+    },
+    Mutation: {
+        setAboutMessage,
+    },
+};
+
+function setAboutMessage(_, { message }) {
+    return aboutMessage = message;
+}
+const server = new ApolloServer({
+    typeDefs: fs.readFileSync('./server/schema.graphql', 'utf-8'),
+    resolvers,
+});
 
 app.use(express.static('public'));
+
+server.applyMiddleware({ app, path: '/graphql' });
 
 app.listen(
     3000, () => {
